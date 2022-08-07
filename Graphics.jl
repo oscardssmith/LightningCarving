528e4ee2045d2aa601228326dffffd0211e77382
Oscar Smith <oscardssmith@gmail.com>
2022-08-07 16:00:06 -0400

    graphics cleanup and algorithm 2

--- Graphics.jl
+++ Graphics.jl
@@ -1,149 +1,154 @@
-import GLFW
+using GLFW
 using ModernGL
+using Images
 
 include("util.jl")
 include("LightningCarving.jl")
 
+function make_window(width=500, height=1000)
+    GLFW.Init()
 
-GLFW.Init()
-width =500
-height =1000
+    window = GLFW.CreateWindow(width, height, "GLFW.jl")
 
-window = GLFW.CreateWindow(width, height, "GLFW.jl")
-
-GLFW.MakeContextCurrent(window)
-GLFW.ShowWindow(window)
-GLFW.SetWindowSize(window, width, height)
-GLFW.SwapInterval(1)
-glViewport(0, 0, width, height)
-println(createcontextinfo())
-# The data for our triangle
-data = GLfloat[
-    -1.0, -1.0,     0.0, 0.0,
-    1.0, -1.0,      1.0, 0.0,
-    1.0,1.0,        1.0, 1.0,
-    -1.0, -1.0,       0.0, 0.0,
-    -1.0, 1.0,      0.0, 1.0,
-    1.0, 1.0,     1.0, 1.0,
-]
-
-
-
-# Generate a vertex array and array buffer for our data
-vao = glGenVertexArray()
-glBindVertexArray(vao)
-vbo = glGenBuffer()
-glBindBuffer(GL_ARRAY_BUFFER, vbo)
-glBufferData(GL_ARRAY_BUFFER, sizeof(data), data, GL_STATIC_DRAW)
-
-
-glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
-glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-texture = glGenTexture()
-
-# Create and initialize shaders
-const vsh = """
-$(get_glsl_version_string())
-in vec2 position;
-in vec2 texcoord;
-
-out vec2 tex;
-void main() {
-    gl_Position = vec4(position, 0.0, 1.0);
-    tex = texcoord;
-}
-"""
-const fsh = """
-$(get_glsl_version_string())
-in vec2 tex;
-
-uniform sampler2D ourTexture;
-out vec4 outColor;
-void main() {
-    outColor = texture(ourTexture, tex);
-}
-"""
-vertexShader = createShader(vsh, GL_VERTEX_SHADER)
-fragmentShader = createShader(fsh, GL_FRAGMENT_SHADER)
-program = createShaderProgram(vertexShader, fragmentShader)
-glUseProgram(program)
-positionAttribute = glGetAttribLocation(program, "position");
-textureAttribute = glGetAttribLocation(program, "texcoord");
-
-glVertexAttribPointer(positionAttribute, 2, GL_FLOAT, false, 4*sizeof(GLfloat), C_NULL)
-glVertexAttribPointer(textureAttribute, 2, GL_FLOAT, false, 4*sizeof(GLfloat), C_NULL+2*sizeof(GLfloat))
-glEnableVertexAttribArray(positionAttribute)
-glEnableVertexAttribArray(textureAttribute)
-
-# Loop until the user closes the window
-
-
-i=0
-mouse_coords = (0,0)
-mouse_down = false
-#push!(a, (5,6))
-
-endpoints = Tuple{Int, Int}[]
-
-function mouse_button_callback(window, button, action, mods)
-    global mouse_down
-    global endpoints
-    println(action)
-    println(button)
-    if button == GLFW.MOUSE_BUTTON_LEFT
-        if action == GLFW.PRESS
-            println("left click")
-            mouse_down = true
-        end
-        if action == GLFW.RELEASE
-            println("released")
-            mouse_down = false
-        end
-    end
-    if button == GLFW.MOUSE_BUTTON_RIGHT
-        if action == GLFW.PRESS
-            println("right click")
-            coords = GLFW.GetCursorPos(window)
-
-            push!(endpoints, (Int(coords.x), Int(coords.y)))
-        end
-    end
-
+    GLFW.MakeContextCurrent(window)
+    GLFW.ShowWindow(window)
+    GLFW.SetWindowSize(window, width, height)
+    GLFW.SwapInterval(1)
+    glViewport(0, 0, width, height)
+    return window
 end
 
-GLFW.SetMouseButtonCallback(window, mouse_button_callback);
+function make_square_and_shader()
+    createcontextinfo()
+    # The data for our triangle
+    data = GLfloat[
+        -1.0, -1.0,     0.0, 0.0,
+        1.0, -1.0,      1.0, 0.0,
+        1.0,1.0,        1.0, 1.0,
+        -1.0, -1.0,       0.0, 0.0,
+        -1.0, 1.0,      0.0, 1.0,
+        1.0, 1.0,     1.0, 1.0,
+    ]
 
-while !GLFW.WindowShouldClose(window)
-    global i += 1
-    global endpoints
-    global mouse_down
-	# Render here
-	glClearColor(0.024, 0.059, 0.231, 1.0)
-	glClear(GL_COLOR_BUFFER_BIT)
+    # Generate a vertex array and array buffer for our data
+    vao = glGenVertexArray()
+    glBindVertexArray(vao)
+    vbo = glGenBuffer()
+    glBindBuffer(GL_ARRAY_BUFFER, vbo)
+    glBufferData(GL_ARRAY_BUFFER, sizeof(data), data, GL_STATIC_DRAW)
+    
+    vsh = """
+    $(get_glsl_version_string())
+    in vec2 position;
+    in vec2 texcoord;
 
-    #lightning_points = Tuple{Int, Int}[]
-    #push!(lightning_points, mouse_coords)
-    @show mouse_down, endpoints
-    if mouse_down && !isempty(endpoints)
-        coords = GLFW.GetCursorPos(window)
-        startpoint = (Int(coords.x), Int(coords.y))
-        lightning = draw_lightning(width, height, startpoint, endpoints)
-    else
-        lightning = draw_empty(width,height)
+    out vec2 tex;
+    void main() {
+        gl_Position = vec4(position, 0.0, 1.0);
+        tex = texcoord;
+    }
+    """
+    fsh = """
+    $(get_glsl_version_string())
+    in vec2 tex;
+
+    uniform sampler2D ourTexture;
+    out vec4 outColor;
+    void main() {
+        outColor = texture(ourTexture, tex);
+    }
+    """
+    vertexShader = createShader(vsh, GL_VERTEX_SHADER)
+    fragmentShader = createShader(fsh, GL_FRAGMENT_SHADER)
+    program = createShaderProgram(vertexShader, fragmentShader)
+    glUseProgram(program)
+    positionAttribute = glGetAttribLocation(program, "position");
+    textureAttribute = glGetAttribLocation(program, "texcoord");
+    glVertexAttribPointer(positionAttribute, 2, GL_FLOAT, false, 4*sizeof(GLfloat), C_NULL)
+    glVertexAttribPointer(textureAttribute, 2, GL_FLOAT, false, 4*sizeof(GLfloat), C_NULL+2*sizeof(GLfloat))
+    glEnableVertexAttribArray(positionAttribute)
+    glEnableVertexAttribArray(textureAttribute)
+    return nothing
+end
+
+function initialize_texture()
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    return glGenTexture()
+end
+
+@Base.kwdef mutable struct LightningState
+    startpoint::Tuple{Int,Int} = (0,0)
+    endpoints::Vector{Tuple{Int, Int}} = Tuple{Int, Int}[]
+    mouse_down::Bool = false
+end
+
+
+function make_mouse_button_callback(state::LightningState)
+    return function mouse_button_callback(window, button, action, mods)
+        println(action)
+        println(button)
+        if button == GLFW.MOUSE_BUTTON_LEFT
+            if action == GLFW.PRESS
+                state.mouse_down = true
+            end
+            if action == GLFW.RELEASE
+                state.mouse_down = false
+            end
+        end
+        if button == GLFW.MOUSE_BUTTON_RIGHT
+            if action == GLFW.PRESS
+                coords = GLFW.GetCursorPos(window)
+                push!(state.endpoints, (round.(Int, coords.x), round.(Int, coords.y)))
+            end
+        end
     end
-    glBindTexture(GL_TEXTURE_2D, texture);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, lightning);
+end
+
+function compute_frame(state, width, height)
+    if isempty(state.endpoints) || !state.mouse_down
+        return draw_empty(width,height)
+    end
+    return draw_lightning(width, height, state.startpoint, state.endpoints)
+end
+
+function bind_matrix_to_texture(matrix::Matrix{RGB{N0f8}}, texture_addr)
+    glBindTexture(GL_TEXTURE_2D, texture_addr);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, size(matrix)..., 0, GL_RGB, GL_UNSIGNED_BYTE, matrix);
     glGenerateMipmap(GL_TEXTURE_2D);
-
-	# Draw our triangle
-	glDrawArrays(GL_TRIANGLES, 0, 6)
-	# Swap front and back buffers
-	GLFW.SwapBuffers(window)
-
-	# Poll for and process events
-	GLFW.PollEvents()
 end
 
-GLFW.DestroyWindow(window)
+function render(compute_frame, width=500, height=1000)
+    window = make_window(width, height)
+    try
+        make_square_and_shader()
+        texture_addr = initialize_texture()
+        state = LightningState()
+        GLFW.SetMouseButtonCallback(window, make_mouse_button_callback(state));
+
+        # Loop until the user closes the window
+        while !GLFW.WindowShouldClose(window)
+            # Render here
+            glClearColor(0.024, 0.059, 0.231, 1.0)
+            glClear(GL_COLOR_BUFFER_BIT)
+            coords = GLFW.GetCursorPos(window)
+            state.startpoint = (round.(Int, coords.x), round.(Int, coords.y))
+            frame = compute_frame(state, width, height)
+            bind_matrix_to_texture(frame, texture_addr)
+
+            # Draw our triangle
+            glDrawArrays(GL_TRIANGLES, 0, 6)
+            # Swap front and back buffers
+            GLFW.SwapBuffers(window)
+
+            # Poll for and process events
+            GLFW.PollEvents()
+        end
+    finally
+        GLFW.DestroyWindow(window)
+    end
+end
+
+render(compute_frame, 500, 1000)
--- LightningCarving.jl
+++ LightningCarving.jl
@@ -1,16 +1,20 @@
 using Images
 
-@views function carve_seam(data)
+function compute_costs(data)
     T = typeof(data)
     data[:, 1] .= Inf
     data[:, end] .= Inf
-    for i in 2:size(data, 1)
+    for i in size(data, 1)-1:-1:1
         for j in 2:size(data, 2)-1
-            data[i, j] += min(data[i-1, j-1], data[i-1, j], data[i-1, j+1])
+            data[i, j] += min(data[i+1, j-1], data[i+1, j], data[i+1, j+1])
         end
     end
-    seam = argmin(data[end, :])
-    for i in size(data,1):-1:1
+    return data
+end
+
+function carve_seam(data)
+    seam = argmin(data[1, :])
+    for i in 1:size(data,1)
         seam += argmin((data[i, seam-1], data[i, seam], data[i, seam+1])) - 2
         data[i, begin:(seam-1)] .= 0
         data[i, seam] = 1
@@ -24,19 +28,18 @@ const light_color = RGB{N0f8}(0.729,0.78,0.835)
 
 draw_empty(width, height) = fill(base_color, width, height)
 function draw_lightning(width, height, start_pt, end_pts)
+    start_pt = reverse(start_pt)
+    end_pts = reverse.(end_pts)
     noise = rand(width, height)'
-    noise[reverse(start_pt)...] = -100_000
-    bolts = zeros(width, height)'
+    
+    noise[start_pt...] = -100_000
     for end_pt in end_pts
-        row_bounds = start_pt[2]:end_pt[2]
-        tmp_noise = copy(noise[row_bounds, :])
-        tmp_noise[end, end_pt[1]] = -100_000
-        carve_seam(tmp_noise)
-        bolts[row_bounds, :] .+= tmp_noise
-        noise[row_bounds, :] .-= .05*tmp_noise
+        noise[end_pt...] = -100_000
     end
+    compute_costs(noise)
+    carve_seam(noise)
 
     img = draw_empty(width, height)
-    img .+= (light_color-base_color).*(bolts'.>0)
+    img .+= (light_color-base_color).*(noise'.>0)
     return img[:, end:-1:begin]
 end
